922168c35dd9ac6c8c21ef3b81b933bd10ee22b4
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-30 02:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function printString(myString) {
   console.log(myString[0]);
-
   if (myString.length > 1) {
     let mySubString = myString.substring(1, myString.length);
     printString(mySubString);
