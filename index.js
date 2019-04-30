2cf447eefc5d73acb26730688264fe6113130ccd
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-30 01:59:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// Code your solution here!
+function printString(myString) {
+  console.log(myString[0]);
 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string) {
+  if(string <= 1) {
+    return true;
+  } else {
+    let substring = string.substring
+  }
+}
