080d249109e93a47fe6a9e24caaa08909bdbdf88
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-30 02:16:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function reverseString(string) {
 }
 
 function isPalindrome(string) {
-  if(string.length > 1) {
+  if(string.length > 1 && string[0] === string[-1]) {
     return isPalindrome()
   } else {
     return false;
