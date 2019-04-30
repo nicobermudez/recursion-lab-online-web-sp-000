8cb24dcef3556fa2f45e2ac378305f35d383b658
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-30 02:03:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function reverseString(string) {
   if(string <= 1) {
     return string;
   } else {
-    string = string + string.substring(0, -1)
+    newString = string + string.substring(0, -1)
     reverseString(newString)
   }
 }
